03b9f6e8727b966b3274207c2a26933102209182
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-06-05 09:42:03 +0200

    Unable to test
    The .js file works by itself, but when testing the html, the CSS, json and Javascript is unable to load into the website, even when running local server.

---
+++ Challenge Project - Random Joke Generator/README.md
@@ -0,0 +1,9 @@
+# Mixed Messages
+
+The purpose of this project was to use javascript, html and css to create a simple webpage, with a button that would print to screen, a random message from an array.
+
+In my solution, I decided for jokes.
+
+### Known bugs
+ - JavaScript works by itself, but unable to load into HTML during testing.
+ - Same problem with CSS stylesheet and json-file.
--- Challenge Project - Random Joke Generator/jokes.js
+++ Challenge Project - Random Joke Generator/jokes.js
@@ -1,18 +1,10 @@
-import data from './jokes.json' assert { type: 'json'};
+import data from './jokes.json';
 
 function getJoke() {
     const jokes = data;
     const rand = Math.floor(Math.random() * jokes.length);
     const text = `"${jokes[rand].joke}"`
-    const jokeHtml = generate_html(text)
-    $('.display').empty().append(jokeHtml)
-    console.log(jokeHtml)
-    //    document.getElementById('display').innerHTML = jokeHtml;
-}
-
-function generate_html(joke) {
-    const jokeHTML = "<i>" + joke + "</i>";
-    return jokeHTML;
+    document.getElementById('display').innerHTML = text;
 }
 
 export default getJoke;
